1bd820dff477d5d052e0084bb7c2e33f9fb1538e
Héctor Salazar <h.salazar509@gmail.com>
2016-07-13 00:43:07 -0500

    Refactor complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  let(:user) { FactoryGirl.build(:user) }
+  subject { user }
+
+  describe '#validations' do    
+    it { is_expected.to be_valid }
+  end
+
+  describe '#associations' do
+    it { is_expected.to have_many :enrollments }
+    it { is_expected.to have_many :courses }
+  end
+
 end
